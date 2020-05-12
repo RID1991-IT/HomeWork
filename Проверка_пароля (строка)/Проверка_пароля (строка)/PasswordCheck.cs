0edfff01493552366b343cad4cbf278a3147bf74
Ilya Ryazantsev <rid1991@yandex.ru>
2020-05-12 22:29:47 +0300

    Строка.Пароль-Логин.Проверка

---
+++ Проверка_пароля (строка)/Проверка_пароля (строка)/PasswordCheck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp16
+{
+    public static class PasswordCheck
+    {
+        private const int passwordLength = 8;
+
+        private const string symbol1 = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        private const string symbol2 = "qwertyuiopasdfghjklzxcvbnm";
+        private const string symbol3 = "!@#$%^&*:;.,-";
+        private const string symbol4 = "1234567890";
+
+        public static bool Check(string password)
+        {
+            password = password.Trim();
+
+            int pass_check1 = password.IndexOfAny(symbol1.ToCharArray());//переводим стринговые переменные в чаровский массив и проверяем есть ли хоть один из представленных символов
+            int pass_check2 = password.IndexOfAny(symbol2.ToCharArray());
+            int pass_check3 = password.IndexOfAny(symbol3.ToCharArray());
+            int pass_check4 = password.IndexOfAny(symbol4.ToCharArray());
+
+            if (password.Length < passwordLength)
+            {
+                return false;
+            }
+            else
+            {
+                if (pass_check1 == -1 || pass_check2 == -1 || pass_check3 == -1 || pass_check4 == -1)//есть indexOFany =-1,значит нет ни одного подходящего сивола
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+    }
+}
---
+++ Проверка_пароля (строка)/Проверка_пароля (строка)/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace ConsoleApp16
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, string> accounts = new Dictionary<string, string>();//словарь,для перечисления унмкальных первых значений и любых других вторых
+
+            string password;
+
+            Write("Введите логин");
+            string login = ReadLine();
+
+            // TO-DO
+            // 1. Проверить логин на повторяемость (логин не должен повторять уже имеющийся)
+            // 2. Проверить пароль на минимальное кол-во символов и требования к символам
+
+            Write("Введите пароль");
+            password = ReadLine();
+            while (!PasswordCheck.Check(password))
+            {
+                WriteLine("Пароль не соответствует требованиям безопасности");
+                Write("Повторите ввод пароля");
+                password = ReadLine();
+            }
+
+            accounts.Add(login, password);
+
+            foreach (var account in accounts)
+            {
+                WriteLine($"{account.Key} - {account.Value}");
+            }
+        }
+    }
+}
